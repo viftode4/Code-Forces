cde7da9b2e3ed1f470b7894997f7a08baff51e22
levladutz <viftode4@yahoo.com>
2021-12-14 18:54:56 +0100

    beginings

---
+++ C - Paint the array/cmake-build-debug/CMakeFiles/C___Paint_the_array.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C___Paint_the_array.dir/main.cpp.obj"
+  "C___Paint_the_array.exe"
+  "C___Paint_the_array.exe.manifest"
+  "C___Paint_the_array.pdb"
+  "libC___Paint_the_array.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/C___Paint_the_array.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ C - Paint the array/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int t, n;
+int a[100000];
+int main() {
+    cin>>t;
+    while(t--)
+    {
+
+    }
+    return 0;
+}
