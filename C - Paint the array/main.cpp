96f978be6d929c297b0835836a7ad6b1bd899018
levladutz <viftode4@yahoo.com>
2021-12-14 19:16:51 +0100

    final

--- C - Paint the array/main.cpp
+++ C - Paint the array/main.cpp
@@ -1,14 +1,41 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int t, n;
-int a[100000];
-int main() {
-    cin>>t;
-    while(t--)
-    {
+long long t, n, gca, gcb;
+long long a[100000];
 
+int main() {
+    cin >> t;
+    while (t--) {
+        cin >> n;
+        cin >> a[0] >> a[1];
+        gca = a[0];
+        gcb = a[1];
+        for (int i = 2; i < n; i++) {
+            cin >> a[i];
+            if (i % 2 == 0)
+                gca = __gcd(gca, a[i]);
+            else gcb = __gcd(gcb, a[i]);
+        }
+        int ok = 1;
+        for (int i = 0; i < n; i += 2)
+            if (a[i] % gcb == 0) {
+                ok = 0;
+                break;
+            }
+
+        if (!ok) {
+            ok = 1;
+            for (int i = 1; i < n; i += 2)
+                if (a[i] % gca == 0) {
+                    cout << "0\n";
+                    ok = 0;
+                    break;
+                }
+            if (ok)
+                cout << gca << '\n';
+        } else cout << gcb << '\n';
     }
     return 0;
 }
